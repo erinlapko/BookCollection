e05912ca151db2859013a1118b4209ba9793e1c6
tamuwicys <102490350+tamuwicys@users.noreply.github.com>
2022-09-15 23:33:47 -0500

    add book attributes and rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_07_044603) do
+ActiveRecord::Schema.define(version: 2022_09_16_042630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.string "author"
+    t.float "price"
+    t.datetime "publisheddate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
